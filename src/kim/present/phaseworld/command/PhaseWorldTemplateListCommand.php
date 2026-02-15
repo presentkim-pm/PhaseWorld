d9375b8a16048c6fe58e6b63574ce82f2ca07d24
PresentKim <debe3721@gmail.com>
2026-02-16 00:12:53 +0900

    docs: Add LICENSE (LGPL v3)

--- src/kim/present/phaseworld/command/PhaseWorldTemplateListCommand.php
+++ src/kim/present/phaseworld/command/PhaseWorldTemplateListCommand.php
@@ -7,8 +7,14 @@
  * |  __/| | |  __/\__ \  __/ | | | |_| . \| | | | | | |
  * |_|   |_|  \___||___/\___|_| |_|\__|_|\_\_|_| |_| |_|
  *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
  * @author       PresentKim (debe3721@gmail.com)
  * @link         https://github.com/PresentKim
+ * @license      https://www.gnu.org/licenses/lgpl-3.0 LGPL-3.0 License
  *
  *   (\ /)
  *  ( . .) ♥
