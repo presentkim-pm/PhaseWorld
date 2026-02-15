1340f9376a32dacfe968f550ac77d5817b65e4b9
PresentKim(김현재) <debe3721@gmail.com>
2026-02-15 23:48:19 +0900

    Initial commit

---
+++ src/authorname/pluginname/Main.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace authorname\pluginname;
+
+use pocketmine\plugin\PluginBase;
+
+final class Main extends PluginBase{
+	protected function onLoad() : void{
+		$this->getLogger()->info("Template plugin loaded!");
+	}
+
+	protected function onEnable() : void{
+		$this->getLogger()->info("Template plugin enabled!");
+	}
+
+	protected function onDisable() : void{
+		$this->getLogger()->info("Template plugin disabled!");
+	}
+}
